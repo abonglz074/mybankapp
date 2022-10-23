87fb8c5e33851823846b4e62f33647f49199f794
a.nazarov <a.nazarov@astondevs.ru>
2022-10-23 22:46:53 +0300

    connect to mongoDb

--- src/main/kotlin/ru/sldbank/Application.kt
+++ src/main/kotlin/ru/sldbank/Application.kt
@@ -1,6 +1,8 @@
 package ru.sldbank
 
 import io.ktor.server.application.*
+import org.litote.kmongo.coroutine.coroutine
+import org.litote.kmongo.reactivestreams.KMongo
 import ru.sldbank.plugins.*
 
 fun main(args: Array<String>): Unit =
@@ -8,6 +10,12 @@ fun main(args: Array<String>): Unit =
 
 @Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
 fun Application.module() {
+    val mongoPassword = System.getenv("MONGO_PASSWORD")
+    val dbName = "sldbank"
+    val db = KMongo.createClient(
+        connectionString = "mongodb+srv://abonglz073:$mongoPassword@cluster0.fnyy1du.mongodb.net/$dbName?retryWrites=true&w=majority"
+    ).coroutine.getDatabase(dbName)
+
     configureSecurity()
     configureSerialization()
     configureMonitoring()
